caac31577f8e1cc4b5f9ff827bd9c7ba39e2b0d9
calvinlfer <calvin.l.fer@outlook.com>
2017-05-06 14:27:48 -0400

    added a comment about SSL - looks like this process isn't integrated into Quill, you need to interact with the underlying ClusterBuilder itself

--- app/repositories/interpreters/CassandraAsyncContextProvider.scala
+++ app/repositories/interpreters/CassandraAsyncContextProvider.scala
@@ -7,5 +7,6 @@ import io.getquill.{CassandraAsyncContext, SnakeCase}
 class CassandraAsyncContextProvider extends Provider[CassandraAsyncContext[SnakeCase]] {
   override def get(): CassandraAsyncContext[SnakeCase] =
     // read from the db object in application.conf
+    // you need to use a slightly different constructor if you want to use SSL
     new CassandraAsyncContext[SnakeCase]("db")
 }
